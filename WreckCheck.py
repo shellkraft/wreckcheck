f0735a0c2673b18bcd99311a4c80f0f8914faf32
B4PH∅M3T <89618500+B4PHOM3T@users.noreply.github.com>
2023-04-08 23:49:45 +0530

    Update WreckCheck.py

--- WreckCheck.py
+++ WreckCheck.py
@@ -1,92 +1,94 @@
 import re
 import sys
-import pyfiglet
+from colorama import Fore
+import time
+import requests
 
-ascii_banner = pyfiglet.figlet_format("WreckCheck")
-print(ascii_banner)
-print("Version: 1.0")
-print("Created by: B4PHOM3T")
-
-
-# Get file path from user input
-while True:
-    try:
-        file_path = input("\n[*] Enter the file path of the code you want to check: ")
-        with open(file_path, 'r') as file:
-            code = file.read()
-        break
-    except FileNotFoundError:
-        print("[-] Error: File not found. Please enter a valid file path.")
-    except Exception as e:
-        print("[-] An error occurred while opening the file.:")
-        print(e)
-        print("Make sure you are providing the correct file path.")
-    except KeyboardInterrupt:
-        print("\n[-] Keyboard interrupt detected. Exiting...")
-        sys.exit(1)
+# banner
+print(Fore.CYAN + '''
+██╗    ██╗██████╗ ███████╗ ██████╗██╗  ██╗ ██████╗██╗  ██╗███████╗ ██████╗██╗  ██╗
+██║    ██║██╔══██╗██╔════╝██╔════╝██║ ██╔╝██╔════╝██║  ██║██╔════╝██╔════╝██║ ██╔╝
+██║ █╗ ██║██████╔╝█████╗  ██║     █████╔╝ ██║     ███████║█████╗  ██║     █████╔╝ 
+██║███╗██║██╔══██╗██╔══╝  ██║     ██╔═██╗ ██║     ██╔══██║██╔══╝  ██║     ██╔═██╗ 
+╚███╔███╔╝██║  ██║███████╗╚██████╗██║  ██╗╚██████╗██║  ██║███████╗╚██████╗██║  ██╗
+ ╚══╝╚══╝ ╚═╝  ╚═╝╚══════╝ ╚═════╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚══════╝ ╚═════╝╚═╝  ╚═╝                                                                
+''' + Fore.MAGENTA + '''                            DEV: B4PH∅M3T | VER: 1.0
+''')
 
 
-# Function to check for vulnerabilities using a regular expression
-def check_vulnerabilities(regex, message):
-    matches = re.finditer(regex, code)
-    result = ""
-    for match in matches:
-        line_number = code[:match.start()].count('\n') + 1
-        result += f"[!] Potential {message} vulnerability found in line {line_number}: {match.group(0)}\n"
-    if result:
-        return result
-    else:
-        return f"[OK] No potential {message} vulnerabilities found in code."
+def check_for_vulnerabilities(code):
+    print('''[*] Checking code for vulnerabilities...
+    ''')
+    time.sleep(1)
+
+    vulnerabilities = {
+        "Input Validation": {
+            "url": "https://cwe.mitre.org/data/definitions/20.html",
+            "regex": r"[^a-zA-Z0-9\s]"
+        },
+        "Buffer Overflow": {
+            "url": "https://owasp.org/www-community/vulnerabilities/Buffer_Overflow",
+            "regex": r"(strcpy|strncpy|sprintf|vsprintf|memcpy|memmove|gets|scanf)\("
+        },
+        "Format String Attack": {
+            "url": "https://owasp.org/www-community/attacks/Format_string_attack",
+            "regex": r"%[0-9]*[a-z]"
+        },
+        "Command Injection": {
+            "url": "https://owasp.org/www-community/attacks/Command_Injection",
+            "regex": r"(\b((?:nc|ftp|sh|ssh|ping|telnet|ncat|nc.traditional|ncat.traditional|curl)\s.*[;&|]))"
+        },
+        "SQL Injection": {
+            "url": "https://owasp.org/www-community/attacks/SQL_Injection",
+            "regex": r"(execute|executemany|SELECT|INSERT INTO|UPDATE|DELETE FROM|WHERE|DROP TABLE|UNION|'OR'|\-\-)\("
+        },
+        "Cross-site Scripting (XSS)": {
+            "url": "https://owasp.org/www-community/attacks/xss/",
+            "regex": r"(<script|</script|document\.cookie|alert\()",
+        },
+        "Authentication": {
+            "url": "https://owasp.org/www-project-top-ten/2017/A3_2017-Sensitive_Data_Exposure",
+            "regex": r"(password|secret|token)\s*=\s*[\"\'][a-zA-Z0-9]*[\"\']"
+        }
+    }
+
+    with open(code, "r") as f:
+        code = f.read()
+
+    vulnerabilities_found = False
+
+    for name, data in vulnerabilities.items():
+        regex = data["regex"]
+        url = data["url"]
+
+        if re.search(regex, code):
+            print(Fore.RED + f"\n[!] Potential {name} vulnerability found in code.")
+            print(Fore.LIGHTYELLOW_EX + f"\tFor more information, see: {url}")
+            lines = [i + 1 for i, x in enumerate(code.split("\n")) if re.search(regex, x)]
+            print(Fore.RED + f"\t[!] Vulnerable line(s): {lines}")
+            vulnerabilities_found = True
+        else:
+            print(Fore.LIGHTGREEN_EX + f"\n[+] No potential {name} vulnerabilities found in code.")
+            print(Fore.LIGHTYELLOW_EX + f"\t[+] For more information, see: {url}")
+
+    if not vulnerabilities_found:
+        print(Fore.LIGHTGREEN_EX + "[+] No potential vulnerabilities found in code.")
+
+    print(Fore.LIGHTCYAN_EX + "\n[*] Vulnerability scan complete.")
 
 
-# Functions to check for specific vulnerabilities
-def check_input_validation_vulnerability():
-    regex = r"gets\("
-    message = "input validation"
-    return check_vulnerabilities(regex, message)
-
-
-def check_buffer_overflow_vulnerability():
-    regex = r"(strcpy|strcat|sprintf|vsprintf)\("
-    message = "buffer overflow"
-    return check_vulnerabilities(regex, message)
-
-
-def check_format_string_vulnerability():
-    regex = r"printf\("
-    message = "format string"
-    return check_vulnerabilities(regex, message)
-
-
-def check_command_injection_vulnerability():
-    regex = r"(popen|system|exec)\("
-    message = "command injection"
-    return check_vulnerabilities(regex, message)
-
-
-def check_sql_injection_vulnerability():
-    regex = r"(\.execute\(|\.executemany\(|SELECT\s.*WHERE\s.*=\s*[\"']?\+\s*)"
-    message = "SQL injection"
-    return check_vulnerabilities(regex, message)
-
-
-def check_xss_vulnerability():
-    regex = r"document\.write\(|innerHTML\s*=\s*|\".*?\";?\s*\+\s*.*?\s*\+\s*.*?\s*\+"
-    message = "cross-site scripting (XSS)"
-    return check_vulnerabilities(regex, message)
-
-
-def check_authentication_vulnerability():
-    regex = r"(password|passwd|secret)\s*=\s*[\"'].*?[\"']"
-    message = "authentication"
-    return check_vulnerabilities(regex, message)
-
-
-# Call the vulnerability checking functions and print the results
-print(check_input_validation_vulnerability())
-print(check_buffer_overflow_vulnerability())
-print(check_format_string_vulnerability())
-print(check_command_injection_vulnerability())
-print(check_sql_injection_vulnerability())
-print(check_xss_vulnerability())
-print(check_authentication_vulnerability())
+if __name__ == "__main__":
+    while True:
+        try:
+            file_path = input(Fore.LIGHTCYAN_EX + "[*] Enter the file path of the code you want to check: ").strip('"')
+            check_for_vulnerabilities(file_path)
+            break
+        except FileNotFoundError:
+            print(Fore.RED + "[-] Error: File not found. Please enter a valid file path.")
+        except Exception as e:
+            print(Fore.RED + "[-] An error occurred while opening the file.:")
+            print(e)
+            print("[!] Make sure you are providing the correct file path.")
+        except KeyboardInterrupt:
+            print(Fore.RED + "\n[-] Keyboard interrupt detected. Exiting...")
+            sys.exit(1)
